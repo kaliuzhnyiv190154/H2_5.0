e6183dc04bbb7418d494fbdafbf65a7afb7cb550
vadym kaliuzhnyi <kaliuzhnyiv190154@sus.htl-grieskirchen.at>
2022-09-26 10:27:52 +0200

    4.0 - All Classes

---
+++ untitled/src/NumberTester.java
@@ -0,0 +1,25 @@
+public class NumberTester {
+    private NumberTester oddEvenTester;
+    private NumberTester primeTester;
+    private NumberTester palindromeTester;
+
+    public NumberTester(String file){
+
+    }
+
+    public void setOddEvenTester(NumberTester oddEvenTester){
+        this.oddEvenTester = oddEvenTester;
+    }
+
+    public void setPrimeTester(NumberTester primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+
+    }
+}
